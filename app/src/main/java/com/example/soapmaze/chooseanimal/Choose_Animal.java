ccb138bb5fc0b8c219eec954b3275d67e264f559
soapmaze <lee141567@naver.com>
2016-11-18 11:49:20 +0900

    11/18 수업자료#2 레이아웃 완성!

---
+++ app/src/main/java/com/example/soapmaze/chooseanimal/Choose_Animal.java
@@ -0,0 +1,13 @@
+package com.example.soapmaze.chooseanimal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Choose_Animal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose__animal);
+    }
+}
